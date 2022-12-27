c3179811d769780980c9141162132b61b87894b5
ishouldkms <wakkusus@proton.me>
2022-12-27 21:10:50 +0000

    creted minu command

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord.ext import commands
 
@@ -13,14 +14,27 @@ bot = commands.Bot(
     intents=intents
 )
 
+
 @bot.event
 async def on_ready():
     print(f"logged in as {bot.user} (ID: {bot.user.id})")
     print("----------")
 
+
 @bot.command()
-async def add(ctx: commands.Context, left: int, right:int):
-    """Adds 2 numbers toghether."""
+async def add(ctx: commands.Context, left: int, right: int):
+    """Adds 2 numbers together."""
     await ctx.send(str(left + right))
+
+
+@bot.command()
+async def choose(ctx: commands.Context, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def minus(ctx: commands.Context, left: int, right: int):
+    await ctx.send(str(left - right))
+
 
 bot.run("TOKEN")
