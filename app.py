3c5aef2b4c8239b237a56747447e4999452d5b40
ishouldkms <wakkusus@proton.me>
2023-01-09 13:18:21 +0000

    Created the 'weather' command

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import random
+import requests
+import json
 import discord
 from discord.ext import commands
 
@@ -22,6 +23,7 @@ async def on_ready():
 
 
 @bot.command()
+@commands.cooldown(3, 5, commands.BucketType.default)
 async def add(ctx: commands.Context, left: int, right: int):
     try:
         result = left + right
@@ -31,6 +33,7 @@ async def add(ctx: commands.Context, left: int, right: int):
 
 
 @bot.command()
+@commands.cooldown(3, 5, commands.BucketType.default)
 async def minus(ctx: commands.Context, left: int, right: int):
     try:
         result = left - right
@@ -38,4 +41,39 @@ async def minus(ctx: commands.Context, left: int, right: int):
         await ctx.send("Error: both arguments should be integers")
     await ctx.send(result)
 
+@bot.command()
+@commands.cooldown(3, 5, commands.BucketType.default)
+async def weather(ctx : commands.Context, city_Name: str, state_Code: commands.Argument = "",country_Code: commands.Argument = ""):
+    endpoint = "http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={appid}"
+    api_key = "{API_KEY}"
+    params = {
+        "city": city_Name,
+        "state": state_Code,
+        "country": country_Code,
+        "format": "json",
+        "appid": api_key,
+
+    }
+    endpoint = "http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={appid}"
+    url = endpoint.format(**params)
+    response = requests.get(url)
+    data = json.loads(response.text)
+
+    params.clear()
+    element = data[0]
+    params = {
+        "lat": element["lat"],
+        "lon": element["lon"],
+        "api_key": api_key,
+    }
+
+    endpoint = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+    url = endpoint.format(**params)
+    response = requests.get(url)
+    data = json.loads(response.text)
+    element = data[0]
+
+    embed = discord.Embed(title=f"Wheather in {city_Name}", description=element["description"])
+    embed.add_field(name="Temperature", value=f"Cº{element['main']}")
+    embed.add_field(name="Wind speed", value=f"KM/h {speed}")
 bot.run("TOKEN")
